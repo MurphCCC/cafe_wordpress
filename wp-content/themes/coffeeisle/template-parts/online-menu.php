b57f7f6b71927a365aeff38a59041ed8ddb651c6
Mike Conrad <mkcnrd@gmail.com>
2017-10-07 09:48:59 -0400

    Restyled background and header image to better work with side navigation

--- wp-content/themes/coffeeisle/template-parts/online-menu.php
+++ wp-content/themes/coffeeisle/template-parts/online-menu.php
@@ -11,7 +11,7 @@
  * Template Name: Online Menu
  */
 
-include 'online-menu-header.php' ?>
+include 'online-menu-header.php'; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
